479d8c939b19f59f07972e514ee526083cb567e8
Piyush Purang <ppurang@gmail.com>
2014-05-31 15:45:35 +0200

    reformat code and move unchanging parts on top

--- src/main/scala/org/purang/net/httpize/package.scala
+++ src/main/scala/org/purang/net/httpize/package.scala
@@ -9,16 +9,24 @@ import Header.`Content-Type`.`application/json`
 
 
 package object httpize {
+
+  implicit def TWritable[T](implicit charset: CharacterSet = CharacterSet.`UTF-8`, encode: EncodeJson[T]) =
+    new SimpleWritable[T] {
+      def contentType: `Content-Type` = `application/json`.withCharset(charset)
+
+      def asChunk(t: T) = ByteVector.view(encode(t).nospaces.getBytes(charset.charset))
+    }
+
   case class IP(remote: String, `x-forwarded-for`: String)
 
   object IP {
-   def apply(r: Request): IP = {
-     IP(r.remoteAddr.getOrElse("xx.xx.xx.xx"),
-       s"${r.headers.get(org.http4s.Header.`X-Forwarded-For`)}")
-   }
+    def apply(r: Request): IP = {
+      IP(r.remoteAddr.getOrElse("xx.xx.xx.xx"),
+        s"${r.headers.get(org.http4s.Header.`X-Forwarded-For`)}")
+    }
 
     implicit def IPCodecJson: CodecJson[IP] =
-casecodec2(IP.apply, IP.unapply)("remote", "x-forwarded-for")
+      casecodec2(IP.apply, IP.unapply)("remote", "x-forwarded-for")
   }
 
   case class UserAgent(`user-agent`: String)
@@ -27,12 +35,8 @@ casecodec2(IP.apply, IP.unapply)("remote", "x-forwarded-for")
     def apply(r: Request): UserAgent = {
       UserAgent(r.headers.get(org.http4s.Header.`User-Agent`).toString)
     }
-    implicit def UserAgentCodecJson: CodecJson[UserAgent] = casecodec1(UserAgent.apply(_ : String), UserAgent.unapply)("user-agent")
+
+    implicit def UserAgentCodecJson: CodecJson[UserAgent] = casecodec1(UserAgent.apply(_: String), UserAgent.unapply)("user-agent")
   }
 
-  implicit def TWritable[T](implicit charset: CharacterSet = CharacterSet.`UTF-8`, encode: EncodeJson[T]) =
-      new SimpleWritable[T] {
-        def contentType: `Content-Type` = `application/json`.withCharset(charset)
-        def asChunk(t: T) = ByteVector.view(encode(t).nospaces.getBytes(charset.charset))
-      }
 }
