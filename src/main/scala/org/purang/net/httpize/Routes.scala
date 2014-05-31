726cb7539333477b1a8be2b76749e80e4c69fc46
Piyush Purang <ppurang@gmail.com>
2014-05-31 15:01:38 +0200

    index html + other static files with modified since handling and js min handling (based on http4s_demo)

--- src/main/scala/org/purang/net/httpize/Routes.scala
+++ src/main/scala/org/purang/net/httpize/Routes.scala
@@ -2,19 +2,29 @@ package org.purang.net.httpize
 
 import org.http4s.dsl._
 import org.http4s._
-import org.http4s.Status.{EntityResponseGenerator, Ok}
+import org.http4s.Status._
 
 import com.typesafe.scalalogging.slf4j.LazyLogging
+import org.http4s.dsl./
 import scalaz.concurrent.Task
-import Header.`Content-Type`.`application/json`
-
 
 class Routes extends LazyLogging {
+
+  val cache = new ResourceCache
+
 
   val service: HttpService = {
     case Get -> Root / "hello" => Ok("Hello world!")
 
     case r @ Get -> Root / "ip" =>  Ok(IP(r))
+
+    case r if r.pathInfo.endsWith(".html")  => cache.getResource("", r.pathInfo, r)
+
+    case r if (r.pathInfo.startsWith("/js") || r.pathInfo.startsWith("/img") || r.pathInfo.startsWith("/css"))  => cache.getResource("", r.pathInfo, r)
+
+    case r if r.pathInfo.endsWith("/") => service(r.withPathInfo(r.pathInfo + "index.html"))
+
+    case r => NotFound("404 Not Found: " + r.pathInfo)
   }
 
 }
